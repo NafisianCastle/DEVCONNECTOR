27b2b62b3322dbaeba709f3758f49bcc9eeb7223
NafisianCastle <nafis.cseaiub@gmail.com>
2022-06-13 15:11:57 +0600

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,64 @@
 const express = require("express");
 const router = express.Router();
-
-//@route GET api/auth
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const User = require("../../models/Users");
+const bcrypt = require("bcryptjs");
+//@route POST api/users
 //@desc Test route
 //@access Public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Plese enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({
+          errors: [{ msg: "User already exists" }],
+        });
+      }
+
+      //get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      //encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      //return jsonwebtoken
+      // console.log(req.body);
+      res.send("User registered");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server erorr");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 
+//connect mongo db
 connectDB();
+
+//init middleware
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 app.get("/", (req, res) => res.send("API running successfully"));
 
 //define routes
-app.get("/api/users", require("./routes/api/users"));
-app.get("/api/profile", require("./routes/api/profile"));
-app.get("/api/posts", require("./routes/api/posts"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

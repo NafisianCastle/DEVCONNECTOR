c8b0850fb9d2502a0b4c09c9ead444dc76864f06
NafisianCastle <nafis.cseaiub@gmail.com>
2022-06-13 13:02:26 +0600

    added routes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
+connectDB();
+
 app.get("/", (req, res) => res.send("API running successfully"));
+
+//define routes
+app.get("/api/users", require("./routes/api/users"));
+app.get("/api/profile", require("./routes/api/profile"));
+app.get("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
